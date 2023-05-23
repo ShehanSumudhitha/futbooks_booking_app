92a9f08103baa191f34fdd417e2d2fee514ecaf4
Shehan <shehan92002@gmail.com>
2023-05-23 23:35:02 +0530

    Final Commit

---
+++ lib/Services/global_variables.dart
@@ -0,0 +1,7 @@
+
+String loginUrlImage = 'https://cdn.pixabay.com/photo/2016/11/29/09/16/architecture-1868667_960_720.jpg';
+String signUpUrlImage = 'https://images.pexels.com/photos/257636/pexels-photo-257636.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+String forgetUrlImage = 'https://images.unsplash.com/photo-1536148935331-408321065b18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQ1fHxjb21wYW55fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60';
+String? name = '';
+String? userImage = '';
+String? location = '';
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'user_state.dart';
+
+void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot)
+      {
+        if(snapshot.connectionState == ConnectionState.waiting)
+          {
+            return const MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Text('iJob clone app is being initialized',
+                  style: TextStyle(
+                    color: Colors.cyan,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+                ),
+              ),
+            );
+          }
+        else if(snapshot.hasError) {
+            return const MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: Text(
+                    'iJob clone app is being initialized',
+                    style: TextStyle(
+                      color: Colors.cyan,
+                      fontSize: 38,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Signatra'
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'iJob Clone App',
+          theme: ThemeData(
+            scaffoldBackgroundColor: Colors.black,
+            primarySwatch: Colors.blue,
+          ),
+          home: UserState(),
+        );
+        });
+  }
+}
